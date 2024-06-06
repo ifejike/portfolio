0f36ec652c68967174c4a59889cf1ae3d0c97649
ifejike <ifeanyiejike107@gmail.com>
2024-06-06 13:14:44 +0100

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'foam-seagreen': '#93e9be',
+      },
+    },
+  },
+  plugins: [],
+}
+
